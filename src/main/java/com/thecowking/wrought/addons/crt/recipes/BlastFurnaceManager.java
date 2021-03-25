092c728e2edfc725301c5bbea8df6b06bb3e5460
Derrek <derreksherman@protonmail.com>
2021-03-24 18:05:56 -0700

    added crt to bloomery and blast furnace

--- src/main/java/com/thecowking/wrought/addons/crt/recipes/BlastFurnaceManager.java
+++ src/main/java/com/thecowking/wrought/addons/crt/recipes/BlastFurnaceManager.java
@@ -17,38 +17,45 @@ import org.openzen.zencode.java.ZenCodeType;
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("rawtypes")
+
 @ZenRegister
-@ZenCodeType.Name("mods.wrought.honey_comb_coke_oven")
-public class CokeOvenManager implements IRecipeManager {
-
-    public CokeOvenManager()  {}
-
+@ZenCodeType.Name("mods.wrought.blast_furnace")
+public class BlastFurnaceManager implements IRecipeManager {
     @Override
     public IRecipeType getRecipeType() {
-        return RecipeSerializerInit.HONEY_COMB_OVEN_TYPE;
+        return RecipeSerializerInit.BLOOMERY_TYPE;
     }
 
     @ZenCodeType.Method
-    public void addRecipe(String name, IIngredient input, IIngredient output_0, IIngredient output_1, IFluidStack fluidOutput, int burnTime)  {
+    public void addRecipe(String name,
+                          IIngredient input_0, IIngredient input_1, IIngredient input_2,
+                          IIngredient output_0, IIngredient output_1, IIngredient output_2,
+                          IFluidStack fluidOutput_0, IFluidStack fluidOutput_1,
+                          int burnTime, int heat)  {
         name = fixRecipeName(name);
 
         List<Ingredient> itemInputs = new ArrayList<>();
-        itemInputs.add(input.asVanillaIngredient());
+        itemInputs.add(input_0.asVanillaIngredient());
+        itemInputs.add(input_1.asVanillaIngredient());
+        itemInputs.add(input_2.asVanillaIngredient());
 
         List<Ingredient> itemOutputs = new ArrayList<>();
         itemOutputs.add(output_0.asVanillaIngredient());
         itemOutputs.add(output_1.asVanillaIngredient());
+        itemOutputs.add(output_2.asVanillaIngredient());
 
         List<FluidStack> fluidOutputs = new ArrayList<>();
-        fluidOutputs.add(fluidOutput.getInternal());
+        fluidOutputs.add(fluidOutput_0.getInternal());
+        fluidOutputs.add(fluidOutput_1.getInternal());
 
-        WroughtRecipe r = new WroughtRecipe(new ResourceLocation("crafttweaker", name), itemInputs, itemOutputs, fluidOutputs, null, null, burnTime, 0, RecipeSerializerInit.HONEY_COMB_OVEN_RECIPE_TYPE_ID);
+        WroughtRecipe r = new WroughtRecipe(new ResourceLocation("crafttweaker", name), itemInputs, itemOutputs, fluidOutputs, null, null, burnTime, 0, RecipeSerializerInit.BLAST_FURNACE_RECIPE_TYPE_ID);
         CraftTweakerAPI.apply(new ActionAddRecipe(this, r, ""));
     }
+
 
     @ZenCodeType.Method
     public void removeRecipe(String name)  {
         removeByName(name);
     }
+
 }
--- src/main/java/com/thecowking/wrought/addons/crt/recipes/BloomeryManager.java
+++ src/main/java/com/thecowking/wrought/addons/crt/recipes/BloomeryManager.java
@@ -6,6 +6,7 @@ import com.blamejared.crafttweaker.api.fluid.IFluidStack;
 import com.blamejared.crafttweaker.api.item.IIngredient;
 import com.blamejared.crafttweaker.api.managers.IRecipeManager;
 import com.blamejared.crafttweaker.impl.actions.recipes.ActionAddRecipe;
+import com.thecowking.wrought.Wrought;
 import com.thecowking.wrought.init.RecipeSerializerInit;
 import com.thecowking.wrought.recipes.WroughtRecipe;
 import net.minecraft.item.crafting.IRecipeType;
@@ -17,36 +18,32 @@ import org.openzen.zencode.java.ZenCodeType;
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("rawtypes")
 @ZenRegister
-@ZenCodeType.Name("mods.wrought.honey_comb_coke_oven")
-public class CokeOvenManager implements IRecipeManager {
-
-    public CokeOvenManager()  {}
-
+@ZenCodeType.Name("mods.wrought.bloomery")
+public class BloomeryManager implements IRecipeManager {
     @Override
     public IRecipeType getRecipeType() {
-        return RecipeSerializerInit.HONEY_COMB_OVEN_TYPE;
+        return RecipeSerializerInit.BLOOMERY_TYPE;
     }
 
+    //    public WroughtRecipe(ResourceLocation id, List<Ingredient> itemInputs,  List<Ingredient> itemOuputsIngredient, List<FluidStack> fluidOutputs,
+    //                              List<FluidStack> fluidInputs, Ingredient fuel, int burnTime, int heat, ResourceLocation recipeTypeID) {
+
     @ZenCodeType.Method
-    public void addRecipe(String name, IIngredient input, IIngredient output_0, IIngredient output_1, IFluidStack fluidOutput, int burnTime)  {
+    public void addRecipe(String name, IIngredient input_0, IIngredient input_1, IIngredient output_0, IIngredient output_1, int burnTime, int heat)  {
         name = fixRecipeName(name);
 
         List<Ingredient> itemInputs = new ArrayList<>();
-        itemInputs.add(input.asVanillaIngredient());
+        itemInputs.add(input_0.asVanillaIngredient());
+        itemInputs.add(input_1.asVanillaIngredient());
 
         List<Ingredient> itemOutputs = new ArrayList<>();
         itemOutputs.add(output_0.asVanillaIngredient());
         itemOutputs.add(output_1.asVanillaIngredient());
 
-        List<FluidStack> fluidOutputs = new ArrayList<>();
-        fluidOutputs.add(fluidOutput.getInternal());
-
-        WroughtRecipe r = new WroughtRecipe(new ResourceLocation("crafttweaker", name), itemInputs, itemOutputs, fluidOutputs, null, null, burnTime, 0, RecipeSerializerInit.HONEY_COMB_OVEN_RECIPE_TYPE_ID);
+        WroughtRecipe r = new WroughtRecipe(new ResourceLocation("crafttweaker", name), itemInputs, itemOutputs, null, null, null, burnTime, heat, RecipeSerializerInit.BLOOMERY_RECIPE_TYPE_ID);
         CraftTweakerAPI.apply(new ActionAddRecipe(this, r, ""));
     }
-
     @ZenCodeType.Method
     public void removeRecipe(String name)  {
         removeByName(name);
